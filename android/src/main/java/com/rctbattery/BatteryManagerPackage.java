293071a1abd695c1414a4aa0265973f6f02a8ba8
Marut Siripan <marut.siripan@okkami.com>
2018-11-13 16:54:28 +0700

    Fixed Android compatible issue.

--- android/src/main/java/com/rctbattery/BatteryManagerPackage.java
+++ android/src/main/java/com/rctbattery/BatteryManagerPackage.java
@@ -1,6 +1,7 @@
 package com.rctbattery;
 
 import com.facebook.react.ReactPackage;
+import com.facebook.react.bridge.JavaScriptModule;
 import com.facebook.react.bridge.NativeModule;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.uimanager.ViewManager;
@@ -23,5 +24,10 @@ public class BatteryManagerPackage implements ReactPackage {
     modules.add(new BatteryManagerModule(reactContext));
 
     return modules;
+  }
+
+  @Override
+  public List<Class<? extends JavaScriptModule>> createJSModules() {
+    return Collections.emptyList();
   }
 }
